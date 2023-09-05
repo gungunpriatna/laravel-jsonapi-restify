da5c404c32fe822975c0733a572ebb8cdabc0d0c
Gun Gun Priatna <gungun002@ummi.ac.id>
2023-09-05 11:15:19 +0700

    init repo

---
+++ app/Restify/UserRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Restify;
+
+use App\Models\User;
+use Binaryk\LaravelRestify\Http\Requests\RestifyRequest;
+
+class UserRepository extends Repository
+{
+    public static string $model = User::class;
+
+    public function fields(RestifyRequest $request): array
+    {
+        return [
+            field('name')->rules('required'),
+
+            field('email')->storingRules('required', 'unique:users')->messages([
+                'required' => 'This field is required.',
+            ]),
+
+            field('email_verified_at')->storingRules('nullable'),
+        ];
+    }
+}
